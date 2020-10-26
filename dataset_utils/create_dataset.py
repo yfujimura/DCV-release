9b9d8fcd1d61ebad959ea3f80a217d72ac79309b
yfujimura <fujimura@h107.170.236.10.32437.vlan.kuins.net>
2020-10-26 14:39:19 +0900

    upload dataset_utils

---
+++ dataset_utils/create_dataset.py
@@ -0,0 +1,116 @@
+import numpy as np
+import os
+import argparse
+import cv2
+import shutil
+import tqdm
+import statistics
+
+# This code creates a npy file that will be loaded by a data loader.
+# A created npy file contains:
+#   hazy_r: reference frame, which can be specified by the argument "frame".
+#   hazy_s: source image
+#   Ks_Rrs_Kri: 3 x 3 array
+#   Ks_trs: 3d array
+#   sparse_points: w x h x 3 array. Each pixel srores a 3D point obtained by SfM. 
+
+# exmaple:
+# python create_dataset.py -i colmap/shirouma2/images -c shirouma2_sparse/ -o test_data -f 37
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--image_path")
+    parser.add_argument("-c", "--colmap_output_path")
+    parser.add_argument("-o", "--output_path")
+    parser.add_argument("-f", "--frame", type=int)
+    args = parser.parse_args()
+    return args
+
+def resize_sparse_points(sparse_points, out_size):
+    h, w, c = sparse_points.shape
+    scale_x = out_size[0] / w
+    scale_y = out_size[1] / h
+    
+    resized_points = np.zeros((out_size[1], out_size[0], c))
+    for y in range(h):
+        y_scaled = y *scale_y
+        y_scaled = round(y_scaled)
+        for x in range(w):
+            x_scaled = x * scale_x
+            x_scaled = round(x_scaled)
+            
+            if 0 <= x_scaled < out_size[0] and 0 <= y_scaled < out_size[1]:
+                resized_points[y_scaled, x_scaled] = sparse_points[y,x]
+                
+    return resized_points
+    
+    
+
+def create_dataset(image_path, colmap_output_path, output_path, image_size, frame=0, th=0.1, interval=1, scale=0.3):
+    
+    i_sample = 0
+    
+    #if os.path.exists(output_path):
+    #    shutil.rmtree(output_path)
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+    
+    frames = os.listdir(image_path)
+    frames = len(frames)
+    
+    K = np.load(os.path.join(colmap_output_path, "K.npy"))
+    K[0,:] = K[0,:] * image_size[0] / 1920.
+    K[1,:] = K[1,:] * image_size[1] / 1080.
+    
+    vr = frame
+    vs = vr + interval
+    if vs >= frames:
+        vs = vr - interval
+        
+    hazy_r = cv2.cvtColor(cv2.imread(os.path.join(image_path, "{:06}.png".format(vr))), cv2.COLOR_BGR2RGB)
+    hazy_s = cv2.cvtColor(cv2.imread(os.path.join(image_path, "{:06}.png".format(vs))), cv2.COLOR_BGR2RGB)
+    hazy_r = cv2.resize(hazy_r, image_size, interpolation=cv2.INTER_AREA)
+    hazy_s = cv2.resize(hazy_s, image_size, interpolation=cv2.INTER_AREA)
+    hazy_r = hazy_r.astype(np.float32) / 255.
+    hazy_s = hazy_s.astype(np.float32) / 255.
+    
+    Rr = np.load(os.path.join(colmap_output_path, "{:06}".format(vr), "R.npy"))
+    tr = np.load(os.path.join(colmap_output_path, "{:06}".format(vr), "t.npy"))
+    Rs = np.load(os.path.join(colmap_output_path, "{:06}".format(vs), "R.npy"))
+    ts = np.load(os.path.join(colmap_output_path, "{:06}".format(vs), "t.npy"))
+    
+    sparse_points = np.load(os.path.join(colmap_output_path, "{:06}".format(vr), "sparse_points.npy"))
+    sparse_points = resize_sparse_points(sparse_points, image_size)
+    
+    sparse_points = sparse_points * scale
+    tr = tr * scale
+    ts = ts * scale
+   
+    RrT = Rr.T
+    RsT = Rs.T
+    Rrs = np.dot(Rs, RrT)
+    RrT_tr = np.dot(RrT, tr)
+    trs = -np.dot(Rs, RrT_tr) + ts
+    Rrs_Kri = np.dot(Rrs, np.linalg.inv(K))
+    Ks_Rrs_Kri = np.dot(K, Rrs_Kri).astype(np.float32)
+    Ks_trs = np.dot(K, trs).astype(np.float32)
+        
+    sparse_points = sparse_points.astype(np.float32)
+        
+    sample = [hazy_r, hazy_s, Ks_Rrs_Kri, Ks_trs, sparse_points]
+    sample = np.array(sample)
+    np.save(os.path.join(output_path, '{:06}'.format(i_sample)), sample)
+    
+                
+def main():
+    args = parse_args()
+    image_path = args.image_path
+    colmap_output_path = args.colmap_output_path
+    output_path = args.output_path
+    frame = args.frame
+    
+    create_dataset(image_path, colmap_output_path, output_path, (256, 192), frame=frame, interval=5, scale=1)
+        
+
+if __name__ == "__main__":
+    main()   
---
+++ dataset_utils/save_sparse_depth.py
@@ -0,0 +1,147 @@
+import os
+import cv2
+import numpy as np
+import argparse
+from scipy.spatial.transform import Rotation
+import statistics
+import matplotlib.pyplot as plt
+import shutil
+
+# This code creates camera parameters and sparse 3D points from colmap sparse output. 
+# Please specify a colmap projetct path as input.
+# npy files corresponding each input frame will be created under an output path.
+
+# exmaple:
+# python save_sparse_depth.py -o shirouma2_sparse -c colmap/shirouma2
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--out_path")
+    parser.add_argument("-c", "--colmap_project_path")
+    args = parser.parse_args()
+    return args
+
+def get_points2d(fn):
+    img_list = []
+    points2d_list = []
+    R_list = []
+    t_list = []
+    
+    with open(fn, "r") as f:
+        lines = f.readlines()
+        for i in range(1, len(lines), 2):
+            points2d = {}
+            line = lines[i]
+            tokens = line.split(" ")
+            img_idx = int(os.path.splitext(tokens[9])[0])
+            img_list.append(img_idx)
+            
+            q = [float(tokens[2]), float(tokens[3]), float(tokens[4]), float(tokens[1])]
+            rot = Rotation.from_quat(q)
+            R = rot.as_dcm()
+            t = np.array([float(x) for x in tokens[5:8]])
+            R_list.append(R)
+            t_list.append(t)
+            
+            line = lines[i+1]
+            tokens = line.split(" ")
+            tokens = [float(t) for t in tokens]
+            for j in range(0, len(tokens), 3):
+                if tokens[j+2] !=-1:
+                    points2d[int(tokens[j+2])] = [tokens[j], tokens[j+1]]
+                    
+            points2d_list.append(points2d)
+                    
+    return img_list, points2d_list, R_list, t_list
+
+def get_points3d(fn):
+    points3d = {}
+    img_id_list = {}
+    pixel_id_list = {}
+    
+    with open(fn, "r") as f:
+        lines = f.readlines()
+        for i in range(1, len(lines)):
+            line = lines[i]
+            tokens = line.split(" ")
+            idx = int(tokens[0])
+            # [x, y, z, error]
+            point = [float(tokens[1]), float(tokens[2]), float(tokens[3]), float(tokens[7])]
+            points3d[idx] = point
+            img_pixel = tokens[8:]
+            img_pixel = [int(x) for x in img_pixel]
+            img_id = [img_pixel[j] for j in range(0, len(img_pixel), 2)]
+            pixel_id = [img_pixel[j] for j in range(1, len(img_pixel), 2)]
+            img_id_list[idx] = img_id
+            pixel_id_list[idx] = pixel_id
+            
+    return points3d, img_id_list, pixel_id_list
+
+def get_intrinsic(fn):
+    K = np.zeros((3,3))
+    K[2,2] = 1
+    with open(fn, "r") as f:
+        lines = f.read().splitlines()
+        line = lines[1]
+        tokens = line.split(" ")
+        width = int(tokens[2])
+        height = int(tokens[3])
+        focal_length = float(tokens[4])
+        cx = float(tokens[5])
+        cy = float(tokens[6])
+        K[0,0] = focal_length
+        K[1,1] = focal_length
+        K[0,2] = cx
+        K[1,2] = cy
+    return width, height, K
+        
+        
+
+
+def main():
+    args = parse_args()
+    
+    out_root_path = args.out_path
+    colmap_project_path = args.colmap_project_path
+    
+    #if os.path.exists(out_root_path):
+    #    shutil.rmtree(out_root_path)
+    #os.mkdir(out_root_path)
+    if not os.path.exists(out_root_path):
+        os.mkdir(out_root_path)
+    
+    width, height, K = get_intrinsic(os.path.join(colmap_project_path, "cameras.txt"))
+    img_list, points2d_list, R_list, t_list = get_points2d(os.path.join(colmap_project_path, "images.txt"))
+    points3d, img_id_list, pixel_id_list = get_points3d(os.path.join(colmap_project_path, "points3D.txt"))
+    
+    np.save(os.path.join(out_root_path, "K"), K)
+    
+    for img_id in range(0, len(img_list), 1):
+        points2d = points2d_list[img_id]
+        R = R_list[img_id]
+        t = t_list[img_id]
+        
+        feature_points = np.zeros((height,width,3))
+        for j in points3d:
+            if j in points2d:
+                x,y = points2d[j]
+                x = round(x)
+                y = round(y)
+                if x >= width or y >= height:
+                    continue
+                point = np.array(points3d[j][0:3])
+                point = np.dot(R, point) + t
+                feature_points[y,x] = point
+                
+        out_path = os.path.join(out_root_path, "{:06}".format(img_list[img_id]))
+        if not os.path.exists(out_path):
+            os.mkdir(out_path)
+        
+        np.save(os.path.join(out_path, "sparse_points"), feature_points)
+        np.save(os.path.join(out_path, "R"), R)
+        np.save(os.path.join(out_path, "t"), t)
+    
+
+if __name__ == "__main__":
+    main()
