f092744dde94028bd9695d457a024132cbf5a671
yfujimura <fujimura@h107.170.236.10.32437.vlan.kuins.net>
2020-10-29 12:10:23 +0900

    edit download script

--- download_model.sh
+++ download_model.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-wget -O model.tar.gz "https://onedrive.live.com/download?cid=22AA8A9F0CDA7E59&resid=22AA8A9F0CDA7E59%2124709&authkey=ACCVi5JVJ0yJQY4"
+wget -O model.tar.gz "https://onedrive.live.com/download?cid=22AA8A9F0CDA7E59&resid=22AA8A9F0CDA7E59%2124711&authkey=AJFqVi_Y_GAl8u0"
